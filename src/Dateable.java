a2a48925f5913a1b8782b09fddd38ba3840d31e4
SamsonPoon147 <schp41@student.cis.edu.hk>
2023-09-15 11:31:06 +0800

    Hello

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 public class Dateable {
-
     public static void main(String[] args) {
-        
+            System.out.println("Please state your age:");
+            Scanner scanner = new Scanner(System.in);
+            int age = scanner.nextInt();
+            int dateAge = age/2 + 7;
+            System.out.println(age + "-year olds should only date someone who's a minimum of " + dateAge + "-years old");
+
+
     }
 }
+
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class Pages {
 
     public static void main(String[] args) {
-
+        System.out.println("Please state your age?");
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        int readAge = 100 - age;
+        System.out.println("" + age + "-year olds should read at least " + readAge + " pages before giving up on a book.");
     }
 }
